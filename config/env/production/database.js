6e1308a931494b4cb8a8159a6ff5f2fb69d8a3b3
mattscript <ayobamifagoroye@yahoo.com>
2023-05-19 13:13:00 +0100

    fix: db prord

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,17 @@
-const { parse } = require("pg-connection-string");
+//const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  //const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
     connection: {
       client: "postgres",
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi')
       },
       debug: false,
     },

05315dc0c5c2b6e480ad724735346045df64bfb3
mattscript <ayobamifagoroye@yahoo.com>
2023-05-19 03:19:52 +0100

    server

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: 'https://www.resimaster.vercel.app',
+  url: 'https://www.jimaz-test.onrender.com',
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
